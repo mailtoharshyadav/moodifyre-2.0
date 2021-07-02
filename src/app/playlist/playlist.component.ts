abbf0e6853ec990c90e128103ba197e2f5e05f19
harshyadav2707 <43298981+harshyadav2707@users.noreply.github.com>
2021-07-02 10:22:45 +0530

    db

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -11,7 +11,7 @@ export class PlaylistComponent implements OnInit {
   emotion:string
   age:number
   gender:string
-
+  db:any;
   teams$ = this.http.get('http://localhost:3000/api/teams');
 ​
   constructor(private http: HttpClient) { }
@@ -20,6 +20,220 @@ export class PlaylistComponent implements OnInit {
     this.age = parseInt( localStorage.getItem('age'))
     this.gender = localStorage.getItem('gender')
     this.emotion = localStorage.getItem('emotion')
+    this.db  = [{
+      "happy": [
+
+        {
+          "name"     : "Chaiyya Chaiyya",
+          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+          "mood"     : "happy",
+          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+          "duration" : {"sec" :  57}
+        },
+        {
+          "name"     : "Chaiyya Chaiyya",
+          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+          "mood"     : "happy",
+          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+          "duration" : {"sec" :  57}
+        },
+        {
+          "name"     : "Chaiyya Chaiyya",
+          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+          "mood"     : "happy",
+          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+          "duration" : {"sec" :  57}
+        },
+        {
+          "name"     : "Chaiyya Chaiyya",
+          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+          "mood"     : "happy",
+          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+          "duration" : {"sec" :  57}
+        },
+        {
+          "name"     : "Chaiyya Chaiyya",
+          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+          "mood"     : "happy",
+          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+          "duration" : {"sec" :  57}
+        },
+        {
+          "name"     : "Chaiyya Chaiyya",
+          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+          "mood"     : "happy",
+          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+          "duration" : {"sec" :  57}
+        },
+        {
+          "name"     : "Chaiyya Chaiyya",
+          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+          "mood"     : "happy",
+          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+          "duration" : {"sec" :  57}
+        },
+        {
+          "name"     : "Chaiyya Chaiyya",
+          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+          "mood"     : "happy",
+          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+          "duration" : {"sec" :  57}
+        },
+        {
+          "name"     : "Chaiyya Chaiyya",
+          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+          "mood"     : "happy",
+          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+          "duration" : {"sec" :  57}
+        }
+      ],
+    "sad": [
+      {
+        "name"     : "Itni Si Baat",
+      "artist"   : "Arijit Singh, Antara Mitra",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  4 , "sec" :  54 }
+      },
+      {
+        "name"     : "Itni Si Baat",
+      "artist"   : "Arijit Singh, Antara Mitra",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  4 , "sec" :  54 }
+      },
+      {
+        "name"     : "Itni Si Baat",
+      "artist"   : "Arijit Singh, Antara Mitra",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  4 , "sec" :  54 }
+      },
+      {
+        "name"     : "Itni Si Baat",
+      "artist"   : "Arijit Singh, Antara Mitra",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  4 , "sec" :  54 }
+      },
+      {
+        "name"     : "Itni Si Baat",
+      "artist"   : "Arijit Singh, Antara Mitra",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  4 , "sec" :  54 }
+      },
+      {
+        "name"     : "Itni Si Baat",
+      "artist"   : "Arijit Singh, Antara Mitra",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  4 , "sec" :  54 }
+      },
+      {
+        "name"     : "Itni Si Baat",
+      "artist"   : "Arijit Singh, Antara Mitra",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  4 , "sec" :  54 }
+      },
+      {
+        "name"     : "Itni Si Baat",
+      "artist"   : "Arijit Singh, Antara Mitra",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  4 , "sec" :  54 }
+      },
+      {
+        "name"     : "Itni Si Baat",
+      "artist"   : "Arijit Singh, Antara Mitra",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  4 , "sec" :  54 }
+      },
+      {
+        "name"     : "Itni Si Baat",
+      "artist"   : "Arijit Singh, Antara Mitra",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  4 , "sec" :  54 }
+      }
+
+    ],
+    "excited" : [
+      {
+        "name"     : "Believer",
+      "artist"   : "Imagine Dragon",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  3 , "sec" :  24 }
+      },
+      {
+        "name"     : "Believer",
+      "artist"   : "Imagine Dragon",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  3 , "sec" :  24 }
+      },
+      {
+        "name"     : "Believer",
+      "artist"   : "Imagine Dragon",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  3 , "sec" :  24 }
+      },
+      {
+        "name"     : "Believer",
+      "artist"   : "Imagine Dragon",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  3 , "sec" :  24 }
+      },
+      {
+        "name"     : "Believer",
+      "artist"   : "Imagine Dragon",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  3 , "sec" :  24 }
+      },
+      {
+        "name"     : "Believer",
+      "artist"   : "Imagine Dragon",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  3 , "sec" :  24 }
+      },
+      {
+        "name"     : "Believer",
+      "artist"   : "Imagine Dragon",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  3 , "sec" :  24 }
+      },
+      {
+        "name"     : "Believer",
+      "artist"   : "Imagine Dragon",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  3 , "sec" :  24 }
+      },
+      {
+        "name"     : "Believer",
+      "artist"   : "Imagine Dragon",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  3 , "sec" :  24 }
+      },
+      {
+        "name"     : "Believer",
+      "artist"   : "Imagine Dragon",
+      "mood"     : "happy",
+      "location" : "" ,
+      "duration" : {"m":  3 , "sec" :  24 }
+      }
+    ]
+    }];
+    console.log(JSON.stringify(this.db));
   }
 
 }
