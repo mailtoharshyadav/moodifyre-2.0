69f69ad695dc3510e5ce8bf28ce65e5dbc57a492
harshyadav2707 <43298981+harshyadav2707@users.noreply.github.com>
2021-07-02 09:48:30 +0530

    ddd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AlbumComponent } from './album/album.component';
-
+import { CoreService } from './core.service';
 
 @NgModule({
   declarations: [
@@ -15,9 +15,11 @@ import { AlbumComponent } from './album/album.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [CoreService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
-
+import { FormsModule } from '@angular/forms';
+import { CoreService } from '../core.service';
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
+    FormsModule,
     LoginRoutingModule
-  ]
+  ],
+  providers: [ CoreService ]
 })
 export class LoginModule { }
