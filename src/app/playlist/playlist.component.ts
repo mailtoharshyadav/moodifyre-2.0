f5af46ce2a4163bb3e88aa19a2070e9502afca8e
harshyadav2707 <43298981+harshyadav2707@users.noreply.github.com>
2021-07-02 11:13:36 +0530

    db integrate

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -12,82 +12,95 @@ export class PlaylistComponent implements OnInit {
   age:number
   gender:string
   db:any;
+  happy:any;
+  currentmood:string;
+  currentmoodArray:any;
+  currentsong: any;
+  sad:any;
+  excited:any;
   teams$ = this.http.get('http://localhost:3000/api/teams');
 ​
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.currentsong =       {
+      "name"     : "Chaiyya Chaiyya",
+      "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+      "mood"     : "happy",
+      "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+      "duration" : {"min": 5,"sec" :  57}
+    };
+   }
 
   ngOnInit(): void {
     this.age = parseInt( localStorage.getItem('age'))
     this.gender = localStorage.getItem('gender')
     this.emotion = localStorage.getItem('emotion')
-    this.db  = [{
-      "happy": [
+    this.happy = [
 
-        {
-          "name"     : "Chaiyya Chaiyya",
-          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
-          "mood"     : "happy",
-          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
-          "duration" : {"sec" :  57}
-        },
-        {
-          "name"     : "Chaiyya Chaiyya",
-          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
-          "mood"     : "happy",
-          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
-          "duration" : {"sec" :  57}
-        },
-        {
-          "name"     : "Chaiyya Chaiyya",
-          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
-          "mood"     : "happy",
-          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
-          "duration" : {"sec" :  57}
-        },
-        {
-          "name"     : "Chaiyya Chaiyya",
-          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
-          "mood"     : "happy",
-          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
-          "duration" : {"sec" :  57}
-        },
-        {
-          "name"     : "Chaiyya Chaiyya",
-          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
-          "mood"     : "happy",
-          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
-          "duration" : {"sec" :  57}
-        },
-        {
-          "name"     : "Chaiyya Chaiyya",
-          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
-          "mood"     : "happy",
-          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
-          "duration" : {"sec" :  57}
-        },
-        {
-          "name"     : "Chaiyya Chaiyya",
-          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
-          "mood"     : "happy",
-          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
-          "duration" : {"sec" :  57}
-        },
-        {
-          "name"     : "Chaiyya Chaiyya",
-          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
-          "mood"     : "happy",
-          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
-          "duration" : {"sec" :  57}
-        },
-        {
-          "name"     : "Chaiyya Chaiyya",
-          "artist"   : "Sukhwinder Singh, Sapna Awasthi",
-          "mood"     : "happy",
-          "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
-          "duration" : {"sec" :  57}
-        }
-      ],
-    "sad": [
+      {
+        "name"     : "Chaiyya Chaiyya",
+        "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+        "mood"     : "happy",
+        "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+        "duration" : {"min": 5,"sec" :  57}
+      },
+      {
+        "name"     : "Chaiyya Chaiyya",
+        "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+        "mood"     : "happy",
+        "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+        "duration" : {"min": 5,"sec" :  57}
+      },
+      {
+        "name"     : "Chaiyya Chaiyya",
+        "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+        "mood"     : "happy",
+        "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+        "duration" : {"min": 5,"sec" :  57}
+      },
+      {
+        "name"     : "Chaiyya Chaiyya",
+        "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+        "mood"     : "happy",
+        "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+        "duration" : {"sec" :  57}
+      },
+      {
+        "name"     : "Chaiyya Chaiyya",
+        "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+        "mood"     : "happy",
+        "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+        "duration" : {"sec" :  57}
+      },
+      {
+        "name"     : "Chaiyya Chaiyya",
+        "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+        "mood"     : "happy",
+        "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+        "duration" : {"sec" :  57}
+      },
+      {
+        "name"     : "Chaiyya Chaiyya",
+        "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+        "mood"     : "happy",
+        "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+        "duration" : {"sec" :  57}
+      },
+      {
+        "name"     : "Chaiyya Chaiyya",
+        "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+        "mood"     : "happy",
+        "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+        "duration" : {"sec" :  57}
+      },
+      {
+        "name"     : "Chaiyya Chaiyya",
+        "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+        "mood"     : "happy",
+        "location" : "Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+        "duration" : {"sec" :  57}
+      }
+    ];
+    this.sad = [
       {
         "name"     : "Itni Si Baat",
       "artist"   : "Arijit Singh, Antara Mitra",
@@ -159,8 +172,8 @@ export class PlaylistComponent implements OnInit {
       "duration" : {"m":  4 , "sec" :  54 }
       }
 
-    ],
-    "excited" : [
+    ];
+    this.excited = [
       {
         "name"     : "Believer",
       "artist"   : "Imagine Dragon",
@@ -232,28 +245,21 @@ export class PlaylistComponent implements OnInit {
       "duration" : {"m":  3 , "sec" :  24 }
       }
     ]
-    }];
-    console.log(JSON.stringify(this.db));
+    this.switchmood(1);
   }
-
+  playsong(song:any){
+    this.currentsong = song;
+  }
+  switchmood(id:any){
+    if(id==1){
+      this.currentmood="Happy";
+      this.currentmoodArray=this.happy;}
+    else if(id==2){
+      this.currentmood="Sad";
+      this.currentmoodArray=this.sad;}
+    else{
+      this.currentmood="Excited";
+      this.currentmoodArray=this.excited;}
+  }
 }
-/*
-[
-  {
-    "name" : "Song name"
-    "artist" : "singer name"
-    "duration" : "song duration"
-    "mood" : "happy"
-    "location" : ""
-  },
-  {
-    "name" : "Song name"
-    "artist" : "singer name"
-    "duration" : "song duration"
-    "mood" : "sad"
-    "location" : ""
-  },
 
-]
-
-*/
