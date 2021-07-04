3d41e97cb1f7e946bf3fcfc496a22a69baf22560
harshyadav2707 <43298981+harshyadav2707@users.noreply.github.com>
2021-07-04 09:56:27 +0530

    push

--- src/app/detect/detect.component.ts
+++ src/app/detect/detect.component.ts
@@ -16,6 +16,38 @@ export class DetectComponent implements OnInit {
 
   ngOnInit(): void {
 
-    }
 
+    }
+    go(){
+      //<!--  anger fear joy and sadness represent sadness surprise disgust contempt -->
+      let mood = localStorage.getItem('emotion');
+      if(mood == 'joy' || mood == 'happy'){
+        localStorage.setItem('song',JSON.stringify(      {
+          "name"     : "Dance Basanti",
+          "artist"   : "Vishal Dadlani, Anushka Manchanda, Sachin Jigar",
+          "mood"     : "happy",
+          "location" : "../../assets/songs/Dance Basanti - 190Kbps mp3 song Download PagalWorld.com.mp3"   ,
+          "duration" : {"min":  3 , "sec" :  44 }
+          }));
+      }
+      else if(mood == 'sad'){
+        localStorage.setItem('song',JSON.stringify(      {
+          "name"     : "Itni Si Baat",
+          "artist"   : "Arijit Singh, Antara Mitra",
+          "mood"     : "sad",
+          "location" : "../../assets/songs/Itni Si Baat - Arijit Singh - 320Kbps mp3 song Download PagalWorld.com.mp3" ,
+          "duration" : {"min":  4 , "sec" :  54 },
+          }));
+      }
+      else{
+        localStorage.setItem('song',JSON.stringify({
+          "name"     : "One Bottle Down",
+          "artist"   : "Yo Yo Honey Singh",
+          "mood"     : "Excited",
+          "location" : "../../assets/songs/abc.mp3",
+          "duration" : {"min": 5,"sec" :  57}
+        }));
+      }
+      this.router.navigateByUrl(`/playlist`);
+    }
 }
--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -38,18 +38,19 @@ export class PlaylistComponent implements OnInit {
     this.gender = localStorage.getItem('gender')
     this.emotion = localStorage.getItem('emotion')
     this.happy =     [
-      {"name"     : "Chaiyya Chaiyya",
-      "artist"   : "Sukhwinder Singh, Sapna Awasthi",
-      "mood"     : "happy",
-      "location" : "../../assets/songs/Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
-      "duration" : {"min":  6 , "sec" :  57 }
-      },
+
       {
       "name"     : "Dance Basanti",
       "artist"   : "Vishal Dadlani, Anushka Manchanda, Sachin Jigar",
       "mood"     : "happy",
       "location" : "../../assets/songs/Dance Basanti - 190Kbps mp3 song Download PagalWorld.com.mp3"   ,
       "duration" : {"min":  3 , "sec" :  44 }
+      }  ,
+      {"name"     : "Chaiyya Chaiyya",
+      "artist"   : "Sukhwinder Singh, Sapna Awasthi",
+      "mood"     : "happy",
+      "location" : "../../assets/songs/Chaiyya Chaiyya - Shahrukh Khan mp3 song Download PagalWorld.com.mp3",
+      "duration" : {"min":  6 , "sec" :  57 }
       },
       {
       "name"     : "Kar Gayi Chull",
@@ -107,63 +108,63 @@ export class PlaylistComponent implements OnInit {
         "artist"   : "Arijit Singh, Antara Mitra",
         "mood"     : "sad",
         "location" : "../../assets/songs/Itni Si Baat - Arijit Singh - 320Kbps mp3 song Download PagalWorld.com.mp3" ,
-        "duration" : {"m":  4 , "sec" :  54 },
+        "duration" : {"min":  4 , "sec" :  54 },
         },
         {
         "name"     : "Khairiyat",
         "artist"   : "Pritam Chakraborty",
         "mood"     : "sad",
         "location" : "../../assets/songs/Khairiyat (Sad) - Chhichhore Mp3 Song Download PagalWorld.mp3",
-        "duration" : {"m":  4 , "sec" :  40 },
+        "duration" : {"min":  4 , "sec" :  40 },
         },
         {
         "name"     : " Main Dhoondne Ko Zamaane Mein",
         "artist"   : "Arijit Singh",
         "mood"     : "sad",
         "location" : "../../assets/songs/Main Dhoondne Ko Zamaane Mein - Arijit Singh - 320Kbps mp3 song Download PagalWorld.com.mp3" ,
-        "duration" : {"m":  4 , "sec" :  23 },
+        "duration" : {"min":  4 , "sec" :  23 },
         },
         {
         "name"     : "Uska Hi Banana",
         "artist"   : "Arijit Singh",
         "mood"     : "sad",
         "location" : "../../assets/songs/Uska Hi Banana - Arijit Singh - 320Kbps mp3 song Download PagalWorld.com.mp3"  ,
-        "duration" : {"m":  5 , "sec" :  28 },
+        "duration" : {"min":  5 , "sec" :  28 },
         },
         {
         "name"     : "Kaise Hua",
         "artist"   : "Vishal Mishra, Vishal Mishra",
         "mood"	   : "sad",
         "location" : "../../assets/songs/Kaise Hua - Kabir Singh mp3 song Download PagalWorld.com.mp3" ,
-        "duration" : {"m":  3 , "sec" :  54 },
+        "duration" : {"min":  3 , "sec" :  54 },
         },
         {
         "name"     : "Judaai ",
         "artist"   : "Rekha Bhardwaj Arijit Singh , SachinJigar" ,
         "mood"     :  "sad",
         "location" : "../../assets/songs/04 Judaai - Badlapur [Arijit Singh] 320Kbps mp3 song Download PagalWorld.com.mp3"    ,
-        "duration" : {"m":  4 , "sec" :  31 },
+        "duration" : {"min":  4 , "sec" :  31 },
         },
         {
         "name"     : "Bekhayali ",
         "artist"   : "Sachet Tandon, Sachet Parampara",
         "mood"     : "sad",
         "location" : "../../assets/songs/" ,
-        "duration" : {"m":  6 , "sec" :  11 },
+        "duration" : {"min":  6 , "sec" :  11 },
         },
         {
         "name"     : " Tu Hai Ki Nahi",
         "artist"   : "Ankit Tiwari, Ankit Tiwari",
         "mood"     : "sad",
         "location" : "../../assets/songs/"   ,
-        "duration" : {"m":  5 , "sec" :  33 },
+        "duration" : {"min":  5 , "sec" :  33 },
         },
         {
         "name"     : "Pachtaoge",
         "artist"   : "Jaani , Arijit Singh, B Praak",
         "mood"     : "sad",
         "location" : "../../assets/songs/",
-        "duration" : {"m":  3 , "sec" :  36 },
+        "duration" : {"min":  3 , "sec" :  36 },
     }];
     this.excited = [
       {
@@ -178,64 +179,54 @@ export class PlaylistComponent implements OnInit {
       "artist"   : "Imagine Dragon",
       "mood"     : "excited",
       "location" : "../../assets/songs/",
-      "duration" : {"m":  3 , "sec" :  24 },
+      "duration" : {"min":  3 , "sec" :  24 },
       },
       {
       "name"     : "Thunder",
       "artist"   : "Imagine Dragon",
       "mood"     : "excited",
       "location" : "../../assets/songs/",
-      "duration" : {"m":  3 , "sec" :  24 },
+      "duration" : {"min":  3 , "sec" :  24 },
       },
       {
       "name"     : "Dangal",
       "artist"   : "Daler Mehndi,",
       "mood"     : "excited",
       "location" : "../../assets/songs/Dangal-b-Title-Song-(pagalworldsongs.co.in).mp3",
-      "duration" : {"m":  4 , "sec" :  59 },
+      "duration" : {"min":  4 , "sec" :  59 },
       },
       {
       "name"     : "Bhaag Milkha Bhaag",
       "artist"   : "Arif Lohar",
       "mood"     : "excited",
       "location" : "../../assets/songs/05 Bhaag Milkha Bhaag mp3 song Download PagalWorld.com.mp3",
-      "duration" : {"m":  4 , "sec" :  29 },
+      "duration" : {"min":  4 , "sec" :  29 },
       },
       {
       "name"     : "Kar Har Maidaan Fateh",
       "artist"   : "Sukhwinder Singh, Shreya Ghoshal, Vikram Montrose",
       "mood"     : "excited",
       "location" : "../../assets/songs/02 Kar Har Maidaan Fateh - Sanju mp3 song Download PagalWorld.com.mp3",
-      "duration" : {"m":  5 , "sec" :  11 },
+      "duration" : {"min":  5 , "sec" :  11 },
       },
       {
       "name"     : "Jigra",
       "artist"   : "Shashwat Sachdev, Siddharth Basrur, Dev Negi,",
       "mood"     : "excited",
       "location" : "../../assets/songs/Jigra-b-URI-(pagalworldsongs.co.in).mp3",
-      "duration" : {"m":  4 , "sec" : 1 },
+      "duration" : {"min":  4 , "sec" : 1 },
       },{
       "name"     : "Ghamand Kar",
       "artist"   : "Sachet Tandon, Parampara Thakur, Sachet Parampara",
       "mood"     : "excited",
       "location" : "../../assets/songs/Ghamand Kar - Tanhaji mp3 song Download PagalWorld.com.mp3",
-      "duration" : {"m":  4 , "sec" :  42 },
+      "duration" : {"min":  4 , "sec" :  42 },
       },{
       "name"     : "Paintra ",
       "artist"   : "Nucleya, DIVINE, Nucleya",
       "mood"     : "excited",
       "location" : "../../assets/songs/",
-      "duration" : {"m":  5 , "sec" :  11 },
-      },{
-      "name"     : "Song name",
-      "artist"   : "singer name",
-      "mood"     : "excited",
-      "location" : "../../assets/songs/",
-      },{
-      "name"     : "Song name",
-      "artist"   : "singer name",
-      "mood"     : "excited",
-      "location" : "",
+      "duration" : {"min":  5 , "sec" :  11 },
       }
       ];
     this.switchmood(1);
@@ -243,6 +234,7 @@ export class PlaylistComponent implements OnInit {
   playsong(song:any){
     this.currentsong = song;
     localStorage.setItem('song', JSON.stringify(song));
+    //this.currentmood =
     window.location.reload();
   }
   switchmood(id:any){
